2e9b4d7929a8173edfb3456bc4b9c994cc1c68f4
Zywy <piotr@zywolewski.eu>
2021-01-08 02:44:02 +0100

    added lines to connect navs, responsibility, lines draws every time window is resize

--- lines.js
+++ lines.js
@@ -1,1 +1,29 @@
-navs = document.querySelectorAll('.nav');
+
+function drawLines()
+{
+    navs = document.querySelectorAll('.nav');
+    lines = document.querySelectorAll('.line');
+    size = document.querySelector('.nav').offsetWidth;
+
+    MainY = navs[2].offsetTop + size/2 ;
+    MainX = navs[2].offsetLeft + size/2;
+    
+    lines.forEach(line => {
+    
+        line.setAttribute('x1',String(MainX));
+        line.setAttribute('y1',String(MainY));
+    });
+    
+    for(i=0;i<navs.length; i++)
+    {
+    
+        lines[i].setAttribute('x2', String(navs[i].offsetLeft + size/2));
+        lines[i].setAttribute('y2', String(navs[i].offsetTop + size/2));
+    }
+}
+
+drawLines();
+
+
+
+window.addEventListener("resize", drawLines);
